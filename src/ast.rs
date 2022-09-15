b9111fd060e760725de9fc10f9c220ead2fe61f6
David Richey <darichey1@gmail.com>
2022-09-14 20:49:54 -0500

    Add basic arith parsing

--- src/ast.rs
+++ src/ast.rs
@@ -2,4 +2,17 @@
 pub enum Expr {
     Int(i64),
     Var(String),
+    BinOp {
+        left: Box<Expr>,
+        right: Box<Expr>,
+        kind: BinOpKind,
+    }
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub enum BinOpKind {
+    Add,
+    Sub,
+    Mul,
+    Div,
 }
--- src/parser.rs
+++ src/parser.rs
@@ -12,21 +12,148 @@ pub fn parse<'input>(
 
 #[cfg(test)]
 mod tests {
-    use crate::{parser::parse, ast::Expr};
+    use crate::{
+        ast::{BinOpKind, Expr},
+        parser::parse,
+    };
 
     #[test]
     fn parses_ints() {
-        assert_eq!(*parse("0").unwrap(), Expr::Int(0));
-        assert_eq!(*parse("10").unwrap(), Expr::Int(10));
-        assert_eq!(*parse("-37").unwrap(), Expr::Int(-37));
-        assert_eq!(*parse("1337").unwrap(), Expr::Int(1337));
-        assert_eq!(*parse("-0").unwrap(), Expr::Int(-0));
+        assert_eq!(parse("0").unwrap(), Box::new(Expr::Int(0)));
+        assert_eq!(parse("10").unwrap(), Box::new(Expr::Int(10)));
+        assert_eq!(parse("-37").unwrap(), Box::new(Expr::Int(-37)));
+        assert_eq!(parse("1337").unwrap(), Box::new(Expr::Int(1337)));
+        assert_eq!(parse("-0").unwrap(), Box::new(Expr::Int(-0)));
     }
 
     #[test]
     fn parses_vars() {
-        assert_eq!(*parse("x").unwrap(), Expr::Var("x".to_string()));
-        assert_eq!(*parse("foo").unwrap(), Expr::Var("foo".to_string()));
-        assert_eq!(*parse("x'").unwrap(), Expr::Var("x'".to_string()));
+        assert_eq!(parse("x").unwrap(), Box::new(Expr::Var("x".to_string())));
+        assert_eq!(
+            parse("foo").unwrap(),
+            Box::new(Expr::Var("foo".to_string()))
+        );
+        assert_eq!(parse("x'").unwrap(), Box::new(Expr::Var("x'".to_string())));
+    }
+
+    #[test]
+    fn parses_bin_ops() {
+        assert_eq!(
+            parse("1 + 1").unwrap(),
+            Box::new(Expr::BinOp {
+                left: Box::new(Expr::Int(1)),
+                right: Box::new(Expr::Int(1)),
+                kind: BinOpKind::Add
+            })
+        );
+
+        assert_eq!(
+            parse("1 - 1").unwrap(),
+            Box::new(Expr::BinOp {
+                left: Box::new(Expr::Int(1)),
+                right: Box::new(Expr::Int(1)),
+                kind: BinOpKind::Sub
+            })
+        );
+
+        assert_eq!(
+            parse("1 * 1").unwrap(),
+            Box::new(Expr::BinOp {
+                left: Box::new(Expr::Int(1)),
+                right: Box::new(Expr::Int(1)),
+                kind: BinOpKind::Mul
+            })
+        );
+
+        assert_eq!(
+            parse("1 / 1").unwrap(),
+            Box::new(Expr::BinOp {
+                left: Box::new(Expr::Int(1)),
+                right: Box::new(Expr::Int(1)),
+                kind: BinOpKind::Div
+            })
+        );
+
+        assert_eq!(
+            parse("a + b").unwrap(),
+            Box::new(Expr::BinOp {
+                left: Box::new(Expr::Var("a".to_string())),
+                right: Box::new(Expr::Var("b".to_string())),
+                kind: BinOpKind::Add
+            })
+        );
+
+        assert_eq!(
+            parse("a + b + c").unwrap(),
+            Box::new(Expr::BinOp {
+                left: Box::new(Expr::BinOp {
+                    left: Box::new(Expr::Var("a".to_string())),
+                    right: Box::new(Expr::Var("b".to_string())),
+                    kind: BinOpKind::Add
+                }),
+                right: Box::new(Expr::Var("c".to_string())),
+                kind: BinOpKind::Add
+            })
+        );
+
+        assert_eq!(
+            parse("a + b - c").unwrap(),
+            Box::new(Expr::BinOp {
+                left: Box::new(Expr::BinOp {
+                    left: Box::new(Expr::Var("a".to_string())),
+                    right: Box::new(Expr::Var("b".to_string())),
+                    kind: BinOpKind::Add
+                }),
+                right: Box::new(Expr::Var("c".to_string())),
+                kind: BinOpKind::Sub
+            })
+        );
+
+        assert_eq!(
+            parse("a - b + c").unwrap(),
+            Box::new(Expr::BinOp {
+                left: Box::new(Expr::BinOp {
+                    left: Box::new(Expr::Var("a".to_string())),
+                    right: Box::new(Expr::Var("b".to_string())),
+                    kind: BinOpKind::Sub
+                }),
+                right: Box::new(Expr::Var("c".to_string())),
+                kind: BinOpKind::Add
+            })
+        );
+
+        assert_eq!(
+            parse("a * b / c").unwrap(),
+            Box::new(Expr::BinOp {
+                left: Box::new(Expr::BinOp {
+                    left: Box::new(Expr::Var("a".to_string())),
+                    right: Box::new(Expr::Var("b".to_string())),
+                    kind: BinOpKind::Mul
+                }),
+                right: Box::new(Expr::Var("c".to_string())),
+                kind: BinOpKind::Div
+            })
+        );
+
+        assert_eq!(
+            parse("a + b * c - d / e").unwrap(),
+            Box::new(Expr::BinOp {
+                left: Box::new(Expr::BinOp {
+                    left: Box::new(Expr::Var("a".to_string())),
+                    right: Box::new(Expr::BinOp {
+                        left: Box::new(Expr::Var("b".to_string())),
+                        right: Box::new(Expr::Var("c".to_string())),
+                        kind: BinOpKind::Mul
+                    }),
+                    kind: BinOpKind::Add
+                }),
+                right: Box::new(Expr::BinOp {
+                    left: Box::new(Expr::Var("d".to_string())),
+                    right: Box::new(Expr::Var("e".to_string())),
+                    kind: BinOpKind::Div
+                }),
+                kind: BinOpKind::Sub
+            })
+        );
     }
 }
