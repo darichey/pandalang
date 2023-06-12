f2321643633066e5735783ac0784c12b0ac8f473
David Richey <darichey1@gmail.com>
2023-06-11 20:19:19 -0500

    Add println builtin

--- crates/pandalang/src/eval/builtins.rs
+++ crates/pandalang/src/eval/builtins.rs
@@ -8,6 +8,7 @@ lazy_static! {
     static ref BUILTINS: HashMap<&'static str, BuiltinFunction> = {
         let mut m: HashMap<&'static str, BuiltinFunction> = HashMap::new();
         m.insert("str_of_int", str_of_int);
+        m.insert("println", println_);
         m
     };
 }
@@ -23,5 +24,15 @@ pub fn str_of_int(x: Value) -> Result<Value, String> {
     match x {
         Value::Int(expr::Int { n }) => Ok(Value::Str(expr::Str { s: n.to_string() })),
         _ => Err("Not an Int".to_string()),
+    }
+}
+
+pub fn println_(x: Value) -> Result<Value, String> {
+    match x {
+        Value::Str(expr::Str { s }) => {
+            println!("{}", s);
+            Ok(Value::Unit)
+        }
+        _ => Err("Not a Str".to_string()),
     }
 }
