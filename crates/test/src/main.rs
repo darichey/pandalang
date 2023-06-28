c560bbc4c4ea7e46dd1f62a0acbbd4fba3bcc50b
David Richey <darichey1@gmail.com>
2023-06-27 21:17:04 -0500

    Compute 'actual' in trial function

--- crates/test/src/main.rs
+++ crates/test/src/main.rs
@@ -37,17 +37,15 @@ fn get_eval_tests() -> Vec<Trial> {
 fn get_parse_tests(record: bool) -> impl Iterator<Item = Trial> {
     let expr_trials = get_input_sources("inputs/parse/exprs/**/*.panda")
         .into_iter()
-        .map(move |(path, src)| {
-            let actual = format!("{:#?}", pandalang::parser::parse_expr(&src));
-            get_snapshot_trial(path, record, actual)
-        });
+        .map(snapshot_trial(record, |_path, src| {
+            format!("{:#?}", pandalang::parser::parse_expr(&src))
+        }));
 
     let prog_trials = get_input_sources("inputs/parse/progs/**/*.panda")
         .into_iter()
-        .map(move |(path, src)| {
-            let actual = format!("{:#?}", pandalang::parser::parse(&src));
-            get_snapshot_trial(path, record, actual)
-        });
+        .map(snapshot_trial(record, |_path, src| {
+            format!("{:#?}", pandalang::parser::parse(&src))
+        }));
 
     expr_trials.chain(prog_trials)
 }
@@ -55,11 +53,10 @@ fn get_parse_tests(record: bool) -> impl Iterator<Item = Trial> {
 fn get_type_check_tests(record: bool) -> impl Iterator<Item = Trial> {
     get_input_sources("inputs/type_check/exprs/**/*.panda")
         .into_iter()
-        .map(move |(path, src)| {
+        .map(snapshot_trial(record, |_path, src| {
             let ast = *pandalang::parser::parse_expr(&src).unwrap();
-            let actual = format!("{:#?}", pandalang::types::check_to_string(ast));
-            get_snapshot_trial(path, record, actual)
-        })
+            format!("{:#?}", pandalang::types::check_to_string(ast))
+        }))
 }
 
 fn get_input_sources(pattern: &str) -> Vec<(String, String)> {
@@ -74,23 +71,28 @@ fn get_input_sources(pattern: &str) -> Vec<(String, String)> {
         .collect()
 }
 
-// TODO: actual should be computed inside the runner closure... Rust makes this _really_ hard??
-fn get_snapshot_trial(path: String, record: bool, actual: String) -> Trial {
-    Trial::test(path.clone(), move || {
-        let expected_path: PathBuf = format!("{}.expected", path).into();
-        if record {
-            fs::write(expected_path, actual).unwrap();
-            Ok(())
-        } else if expected_path.exists() {
-            let expected = fs::read_to_string(expected_path).unwrap();
-            if expected == actual {
+fn snapshot_trial(
+    record: bool,
+    get_actual: fn(String, String) -> String,
+) -> impl FnMut((String, String)) -> Trial {
+    move |(path, src)| {
+        Trial::test(path.clone(), move || {
+            let actual = get_actual(path.clone(), src);
+            let expected_path: PathBuf = format!("{}.expected", path).into();
+            if record {
+                fs::write(expected_path, actual).unwrap();
                 Ok(())
+            } else if expected_path.exists() {
+                let expected = fs::read_to_string(expected_path).unwrap();
+                if expected == actual {
+                    Ok(())
+                } else {
+                    let diff = SimpleDiff::from_str(&expected, &actual, "expected", "actual");
+                    Err(diff.into())
+                }
             } else {
-                let diff = SimpleDiff::from_str(&expected, &actual, "expected", "actual");
-                Err(diff.into())
+                Err("Couldn't find .expected file. Did you mean to --record it?".into())
             }
-        } else {
-            Err("Couldn't find .expected file. Did you mean to --record it?".into())
-        }
-    })
+        })
+    }
 }
