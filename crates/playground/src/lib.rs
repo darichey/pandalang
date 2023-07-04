06e6fac1711d51480aa8d73794f9467e0f5d0768
David Richey <darichey1@gmail.com>
2023-07-04 18:28:38 -0500

    Show output in playground

--- crates/playground/src/lib.rs
+++ crates/playground/src/lib.rs
@@ -1,8 +1,3 @@
-mod console;
-
-use std::io::Write;
-
-use console::console;
 use pandalang_parser::ast::Program;
 use wasm_bindgen::prelude::wasm_bindgen;
 
@@ -23,10 +18,9 @@ pub fn typecheck(source: &str) -> Result<String, String> {
 pub fn run(source: &str) -> Result<String, String> {
     let ast = parse_(source)?;
     pandalang_types::check_prog_to_strings(ast.clone()).map_err(|err| err.to_string())?;
-    let mut stdout = console();
-    let output = pandalang_eval::run_program(ast, &mut stdout)?;
-    stdout.flush().map_err(|err| err.to_string())?;
-    Ok(format!("{:#?}", output))
+    let mut stdout = Vec::new();
+    pandalang_eval::run_program(ast, &mut stdout)?;
+    Ok(String::from_utf8_lossy(&stdout).to_string())
 }
 
 fn parse_(source: &str) -> Result<Program, String> {
