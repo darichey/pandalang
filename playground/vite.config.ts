444fb1108cccee6bb75b4d77a709dbcb69f22d3a
David Richey <darichey1@gmail.com>
2023-07-05 22:54:05 -0500

    Use topLevelAwait plugin

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import rust from "@wasm-tool/rollup-plugin-rust";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +13,6 @@ export default defineConfig({
         directExports: true,
       },
     }),
+    topLevelAwait(),
   ],
 });
