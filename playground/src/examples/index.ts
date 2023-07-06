22f6f9a53e3c6b95a230c6857c5f7821a5b0149e
David Richey <darichey1@gmail.com>
2023-07-06 01:04:03 -0500

    Add factorial example

--- playground/src/examples/index.ts
+++ playground/src/examples/index.ts
@@ -1,6 +1,7 @@
 const examples: Record<string, string> = {
   "Hello world": (await import("./helloWorld.panda?raw")).default,
   FizzBuzz: (await import("./fizzbuzz.panda?raw")).default,
+  factorial: (await import("./factorial.panda?raw")).default,
 };
 
 export function getExample(name: string): string {
