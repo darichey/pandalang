03c5acd1372fde586d9cad3fab9caa8eb5a39d98
David Richey <darichey1@gmail.com>
2023-07-06 00:46:02 -0500

    Type check before running program in cli

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -24,6 +24,7 @@ fn main() -> Result<(), String> {
         Commands::Run { program } => {
             let src = fs::read_to_string(program).map_err(|err| err.to_string())?;
             let ast = pandalang_parser::parse(&src).map_err(|err| err.to_string())?;
+            pandalang_types::check_prog_to_strings(ast.clone()).map_err(|err| err.to_string())?;
             let mut stdout = std::io::stdout();
             let value = pandalang_eval::run_program(ast, &mut stdout)?;
             println!("{}", value);
